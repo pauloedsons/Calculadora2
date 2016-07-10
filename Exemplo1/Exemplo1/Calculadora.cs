625a25335eebd0abad571095a6492d18f7f8ddaa
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-10 03:58:13 -0300

    CLASSE DE TESTE

---
+++ Exemplo1/Exemplo1/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    public class Calculadora
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Soma (int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
