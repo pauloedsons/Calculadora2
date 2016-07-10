1d7941951131e1650aa18a9af7b007941203748c
Paulo Edson <paulo_edsons@hotmail.com>
2016-07-10 04:51:23 -0300

    Msid um código igual de calculadora

---
+++ ConsoleApplication1/Testes/TestaCalculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exemplo1;
+namespace Testes
+{
+    [TestClass]
+    public class TestaCalculadora
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculadora c = new Calculadora();
+             int resultado = c.Soma(3, 4);
+            Assert.AreEqual(7, resultado);
+        }
+    }
+}
